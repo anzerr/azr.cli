ae563521a6cc9a9d6f82c8a2c06117715829b709
anzerr <ryan.anzer@epitech.eu>
2018-05-29 17:07:15 +0200

    save

--- docker.sh
+++ docker.sh
@@ -9,4 +9,4 @@ add-apt-repository \
    "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
    $(lsb_release -cs) \
    stable" && \
-apt-get update && apt-get install -y docker-ce=17.03
+apt-get update && apt-get install -y docker-ce=17.03.0~ce-0~ubuntu-xenial
