a53ffd395251dc9ba85a565212ddec22b7d8e8c6
anzerr <ryan.anzer@epitech.eu>
2018-11-14 17:56:21 +0100

    fast save

--- project.sh
+++ project.sh
@@ -2,4 +2,6 @@ npm init -f && \
 	echo '' > README.md && \
 	npm i -D eslint && \
 	npm i -D git+ssh://git@github.com/anzerr/eslintrc.git && \
+	echo '' > index.js && \
+	echo 'node_modules\nnode_modules/*\n.tag*\n' > .gitignore && \n
 	node -e "let a = require('./package.json'); a.eslintConfig = {extends: 'eslint-config-basic'}; require('fs').writeFileSync('./package.json', JSON.stringify(a, null, '\t'));"
