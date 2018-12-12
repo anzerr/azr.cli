6fd9642a2922e5bc478c8a18a9227e0229d86a83
anzerr <ryan.anzer@epitech.eu>
2018-12-12 18:59:05 +0100

    update to version 1.0.1

--- macro/src/cli/git.js
+++ macro/src/cli/git.js
@@ -8,12 +8,13 @@ module.exports = (arg, cwd) => {
 			branch = (branch) ? branch[1] : 'master';
 
 			return util.exec('git add .', {cwd: cwd}).then(() => {
-				let name = arg.get() || 'fast save';
+				let name = arg.get() || 'dump';
 				return util.exec('git commit -m "' + name + '"', {cwd: cwd});
 			}).then(() => {
 				return util.exec('git push origin ' + branch, {cwd: cwd});
 			});
 		}).catch(console.log);
 	}
+
 	return false;
 };
--- macro/src/cli/npm.js
+++ macro/src/cli/npm.js
@@ -1,8 +1,34 @@
 
-const util = require('../util.js');
+const util = require('../util.js'),
+	fs = require('fs.promisify'),
+	path = require('path');
 
-module.exports = (arg) => {
+let nextVersion = (cwd) => {
+	return fs.readFile(path.join(cwd, 'package.json')).then((res) => {
+		let json = JSON.parse(res.toString());
+		let version = json.version.split('.');
+		version[version.length - 1] = Number(version[version.length - 1]) + 1;
+		json.version = version.join('.');
+		return json;
+	});
+};
+
+module.exports = (arg, cwd, cli) => {
 	if (arg.is('npm')) {
+		if (arg.is('version')) {
+			return nextVersion(cwd).then((res) => {
+				return fs.writeFile(path.join(cwd, 'package.json'), JSON.stringify(res, null, '\t'));
+			}).catch(console.log);
+		}
+		if (arg.is('push')) {
+			return nextVersion(cwd).then((res) => {
+				return util.exec([
+					'azr npm version',
+					cli.has('clean') ? 'rm -Rf node_modules package-lock.json && npm i' : 'echo "skip new lock"',
+					'azr commit "update to version ' + res.version + '"'
+				].join(' && '));
+			}).catch(console.log);
+		}
 		return util.exec('rm -Rf node_modules package-lock.json && npm i');
 	}
 	return false;
