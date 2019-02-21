08e18faa0e81f81197d16b133e828fe4548e28b3
anzerr <ryan.anzer@epitech.eu>
2019-02-21 18:31:36 +0100

    dump

--- macro/src/cli/static.js
+++ macro/src/cli/static.js
@@ -2,7 +2,7 @@
 const Server = require('static.http');
 
 module.exports = (arg, cwd, cli) => {
-	if (arg.is('yaml')) {
+	if (arg.is('static')) {
 		return new Server(Number(cli.get('port') || 3000), cli.get('cwd') || cwd)
 			.on('log', console.log)
 			.create().then(() => {
