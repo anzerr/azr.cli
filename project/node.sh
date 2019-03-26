78974c92674111ebec378e31dc2fb12528e35af0
anzerr <ryan.anzer@epitech.eu>
2019-03-26 16:52:13 +0100

    dump

--- project/node.sh
+++ project/node.sh
@@ -1,7 +1,7 @@
 npm init -f && \
 	echo $'\n'"### \`Intro\`"$'\n' > README.md && \
 	npm i -D eslint@5.11.1 && \
-	npm i -D git+ssh://git@github.com/anzerr/eslintrc.git && \
+	npm i -D git+http://git@github.com/anzerr/eslintrc.git && \
 	echo '' > index.js && \
 	echo "node_modules"$'\n'"node_modules/*"$'\n'".tag*"$'\n' > .gitignore &&
 	node -e "let a = require('./package.json'); a.eslintConfig = {extends: 'eslint-config-basic'}; require('fs').writeFileSync('./package.json', JSON.stringify(a, null, '\t'));" &&
