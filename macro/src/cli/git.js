4bf55caddc494b32d7c85ce61724210a49f89bef
anzerr <ryan.anzer@epitech.eu>
2019-07-02 15:06:58 +0200

    ✨ added type to commit and push tags

--- macro/src/cli/git.js
+++ macro/src/cli/git.js
@@ -19,7 +19,9 @@ module.exports = (arg, cwd, cli) => {
 			let branch = res.toString().match(/\*\s(.*)/);
 			branch = (branch) ? branch[1] : 'master';
 
+			let package = false;
 			return fs.readFile(path.join(cwd, 'package.json')).then((json) => {
+				package = true;
 				let a = JSON.parse(json.toString());
 				if (a.scripts && a.scripts.build && a.scripts.build.match(/^tsc/)) {
 					if (a.scripts.clean) {
@@ -34,7 +36,7 @@ module.exports = (arg, cwd, cli) => {
 					t = (cli.has('type') ? type[cli.get('type')] : '') || type.wip;
 				return util.exec(`git commit -m "${t} ${name}"`, {cwd: cwd});
 			}).then(() => {
-				if (!cli.has('version')) {
+				if (!cli.has('version') && package) {
 					return util.exec('npm version patch', {cwd: cwd});
 				}
 				return Promise.resolve();
