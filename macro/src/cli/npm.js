acd9a6241f5e00335ca6862c40d93de6e6e4e83c
anzerr <ryan.anzer@epitech.eu>
2019-09-04 11:56:24 +0200

    🚧 dump

--- macro/src/cli/npm.js
+++ macro/src/cli/npm.js
@@ -19,8 +19,113 @@ let nextVersion = (cwd) => {
 	});
 };
 
+let	scan = (dir, filter, out = []) => {
+	if (filter && dir.match(filter)) {
+		return Promise.resolve();
+	}
+	return fs.stat(dir).then(async (res) => {
+		if (res.isDirectory()) {
+			let list = await fs.readdir(dir), p = Promise.resolve();
+			for (let i in list) {
+				((file) => {
+					p = p.then(() => scan(file, filter, out));
+				})(path.join(dir, list[i]));
+			}
+			return p;
+		}
+		out.push(dir);
+	}).then(() => out);
+};
+
 module.exports = (arg, cwd, cli) => {
 	if (arg.is('npm')) {
+		if (arg.is('used')) {
+			const projectPath = path.join(cwd, cli.get('cwd') || '.');
+			let pack = null;
+			return getPackage(cwd).then((res) => {
+				pack = res;
+				const wait = [scan(projectPath, new RegExp(cli.get('filter') || 'node_modules'))];
+
+				for (let i in pack.dependencies) {
+					wait.push(scan(path.join(projectPath, `node_modules/${i}`)));
+				}
+
+				for (let i in pack.devDependencies) {
+					wait.push(scan(path.join(projectPath, `node_modules/${i}`)));
+				}
+
+				return Promise.all(wait);
+			}).then((res) => {
+				return [].concat(...res);
+			}).then((res) => {
+				res = res.filter((a) => a.match(/\.(ts|js)$/));
+				let out = {}, p = Promise.resolve();
+				for (let i in res) {
+					((file) => {
+						p = p.then(() => {
+							return fs.readFile(file);
+						}).then((data) => {
+							let list = (data.toString().match(/import(\s.*?\sfrom){0,1}\s('|")[@a-zA-Z0-9\._\-\\\/]+?('|")/g) || [])
+								.concat((data.toString().match(/require\(('|")([@a-zA-Z0-9\._\-\\\/]+?)('|")\)/g) || []));
+							for (let x in list) {
+								if (list[x]) {
+									try {
+										let k = list[x].match(/('|")(.*?)('|")/)[2];
+										if (!file.match(k)) {
+											if (!out[k]) {
+												out[k] = {
+													count: 0,
+													file: {}
+												};
+											}
+											out[k].count += 1;
+											out[k].file[file.replace(cwd, '').replace(/\\/g, '/')] = true;
+										}
+									} catch (e) {
+									//
+									}
+								}
+							}
+						});
+					})(res[i]);
+				}
+				return p.then(() => {
+					return {pack: {dep: pack.dependencies, dev: pack.devDependencies}, imports: out};
+				});
+			}).then(({pack, imports}) => {
+				for (let i in pack) {
+					for (let x in pack[i]) {
+						pack[i][x] = {count: 0, file: {}};
+					}
+				}
+				for (let i in pack) {
+					for (let x in pack[i]) {
+						let reg = new RegExp(`^${x}($|(\\/|\\\\).*?)`);
+						for (let v in imports) {
+							if (v.match(/^\.{1,2}(\/|\\)/)) {
+								continue;
+							}
+							if (v.match(reg)) {
+								pack[i][x].count += imports[v].count;
+								for (let y in imports[v].file) {
+									pack[i][x].file[y] = imports[v].file[y];
+								}
+							}
+						}
+					}
+				}
+				console.log('\n------    dev    ------');
+				for (let x in pack.dep) {
+					let count = pack.dep[x].count, c = (a) => color[count ? 'green' : 'red'](a);
+					console.log(x, c(pack.dep[x].count), count ? '\n' : '', c(Object.keys(pack.dep[x].file)));
+				}
+				console.log('\n------    dev    ------');
+				for (let x in pack.dev) {
+					let count = pack.dev[x].count, c = (a) => color[count ? 'green' : 'red'](a);
+					console.log(x, c(pack.dev[x].count) + count ? '\n' : '', c(Object.keys(pack.dev[x].file)));
+				}
+			}).catch((err) => console.log(color.red(err)));
+		}
 		if (arg.is('version')) {
 			return nextVersion(cwd).then((res) => {
 				return fs.writeFile(path.join(cwd, 'package.json'), JSON.stringify(res, null, '\t'));
@@ -44,17 +149,26 @@ module.exports = (arg, cwd, cli) => {
 				res.engines = {
 					node: '>= 0.10.0'
 				};
-				if (!res.types) {
-					res.types = res.main.replace(/\.js$/, '.d.ts');
+				if (!res.main) {
+					res.main = 'index.js';
 				}
-				res.author = 'anzerr';
-				res.license = 'MIT';
+				if (!res.types) {
+					res.types = (res.main || 'index.js').replace(/\.js$/, '.d.ts');
+				}
+				if (!res.author) {
+					res.author = 'anzerr';
+				}
+				if (!cli.has('simple')) {
+					res.license = cli.get('license') || 'MIT';
+				}
 				res.bugs = {
 					url: repo.https + '/issues'
 				};
 				res.homepage = repo.https + '#readme';
 				console.log(res);
-				await util.exec('azr license --type MIT', {cwd: cwd});
+				if (!cli.has('simple')) {
+					await util.exec(`azr license --type ${res.license}`, {cwd: cwd});
+				}
 				await fs.writeFile(path.join(cwd, 'package.json'), JSON.stringify(res, null, '\t') + '\n');
 			}).catch((err) => console.log(color.red(err)));
 		}
